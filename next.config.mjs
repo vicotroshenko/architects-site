bf578d341bbc0d953c4a9278e3e673ad7c146f6c
vicotroshenko <otroshenko.vl@gmail.com>
2024-04-22 13:34:55 +0300

    fist commit

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+	basePath: "/architects-site",
+	output: "export", 
+  reactStrictMode: true,
+};
+
+export default nextConfig;
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="w-32 h-32 bg-red-600">
+      
+    </main>
+  );
+}
