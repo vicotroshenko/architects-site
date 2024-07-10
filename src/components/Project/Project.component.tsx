1f088810e673bfd8ec92e85fd96d1176804774b2
vicotroshenko <otroshenko.vl@gmail.com>
2024-07-10 14:06:00 +0300

    fix: Changed project params

--- src/components/Project/Project.component.tsx
+++ src/components/Project/Project.component.tsx
@@ -1,13 +1,13 @@
-'use client';
-
-import { useParams } from 'next/navigation';
 import React from 'react';
 
 import CONTENT from '../../../public/data/projects.json';
 import Container from '../Container/Container.component';
 
-const Project = () => {
-  const { id } = useParams();
+interface ProjectProps {
+  id: string;
+}
+
+const Project: React.FC<ProjectProps> = ({ id }) => {
   const project = CONTENT.projects.find((item) => item.id === id);
   console.log('project: ', project);
 
