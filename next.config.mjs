447edf55153ff3e721eee3e4a8b55e634b06b18e
vicotroshenko <otroshenko.vl@gmail.com>
2024-07-11 18:24:11 +0300

    fix: Change next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    appDir: true,
+    serverComponentsExternalPackages: ['mongoose'],
+  },
   images: {
     deviceSizes: [768, 1170],
     remotePatterns: [
